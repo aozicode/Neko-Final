91819e74c4d459c0fcfe738cde6d36cd7e05b922
prhehns <pmalatuba@gmail.com>
2024-09-28 23:21:32 +0800

    added active link inde=icator

--- src/header.js
+++ src/header.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './css/header.css';
 import logo from './assets/logo/wnlogo.ico';
 
 function Header() {
+    const location = useLocation();
+
+    // Helper function to check if the link is active
+    const isActive = (path) => location.pathname === path;
     return (
         <div className="App">
             <header className="header">
                 <img src={logo} alt="House" />
                 <nav className="navbar"> {/* Changed class to className */}
                     {/* Use Link for internal navigation */}
-                    <Link to="/">Home</Link>
-                    <Link to="/animals">Animals</Link>
-                    <Link to="/About">About Us</Link>
+                    <Link to="/" className={isActive("/") ? "active" : ""}>Home</Link>
+                    <Link to="/animals" className={isActive("/animals") ? "active" : ""}>Animals</Link>
+                    <Link to="/about" className={isActive("/about") ? "active" : ""}>About Us</Link>
                 </nav>
             </header>
         </div>
